897c00966770bea5f9065c661ff3391a28f3fb58
sapphire8 <kaleneha997@gmail.com>
2019-10-29 18:28:48 +1200

    change usernamme

--- app/src/main/java/com/example/spit_app/Change_username.java
+++ app/src/main/java/com/example/spit_app/Change_username.java
@@ -2,13 +2,48 @@ package com.example.spit_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.spit_app.admin.AdminDisplayGeneral;
+import com.example.spit_app.admin.Recycler_Adapter_Admin;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Change_username extends AppCompatActivity {
+    FirebaseAuth firebaseAuth;
+    DatabaseReference myRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_username);
+        final EditText txt_new_username=(EditText)findViewById(R.id.new_username);
+        Button btn= (Button)findViewById(R.id.button_new_username);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseUser user=firebaseAuth.getCurrentUser();
+                String userId=user.getUid();
+                myRef =FirebaseDatabase.getInstance().getReference("Users");
+
+                String newUser = txt_new_username.getText().toString();
+
+                myRef.child(userId).child("ID").setValue(userId);
+                myRef.child(userId).child("Username").setValue(newUser);
+                toastMessage("Username has been changed succesfully!");
+                finish();
+            }
+        });
+    }
+    private void toastMessage(String message){
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/spit_app/Profile.java
+++ app/src/main/java/com/example/spit_app/Profile.java
@@ -3,7 +3,10 @@ package com.example.spit_app;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -26,6 +29,14 @@ public class Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        Button change_username= (Button)findViewById(R.id.button_profile);
+        change_username.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Profile.this, Change_username.class);
+                startActivity(intent);
+            }
+        });
         name=findViewById(R.id.user);
         uid=findViewById(R.id.uidname);
         FirebaseUser users = FirebaseAuth.getInstance().getCurrentUser();
@@ -33,6 +44,7 @@ public class Profile extends AppCompatActivity {
         String uid1=users.getUid();
         profile=FirebaseDatabase.getInstance();
         reference=profile.getReference().child("Users").child(uid1);
+
         reference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
