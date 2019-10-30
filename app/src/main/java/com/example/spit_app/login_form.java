44fd0c115ab1a678bdcc78a84df2618800970de5
sapphire8 <kaleneha997@gmail.com>
2019-10-31 11:38:37 +1200

    change usernamme

--- app/src/main/java/com/example/spit_app/login_form.java
+++ app/src/main/java/com/example/spit_app/login_form.java
@@ -1,15 +1,18 @@
 package com.example.spit_app;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ScrollView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -28,6 +31,13 @@ public class login_form extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_form);
+
+        ScrollView constraintLayout=(ScrollView)findViewById(R.id.login_layout);
+        AnimationDrawable animationDrawable=(AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2000);
+        animationDrawable.setExitFadeDuration(4000);
+        animationDrawable.start();
+
         final String admin = "admin@gmail.com";
 
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
